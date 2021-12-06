23cc5029fd4de4beda1dec9b116255125f361bce
ivolake <bzakhir@gmail.com>
2021-12-06 18:51:22 +0300

    Архитектура певрого расчетного класса
    main.py: класс Step2

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import os
 import pandas as pd
+
+
+class Step2:
+    def __init__(self, step1_df):
+        self.step1_df = step1_df
+
+    def func1(self):
+        pass
+
+    def get_result(self):
+        df = ...
+        return df
