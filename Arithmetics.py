c812a5ce2299c1f40cb6dc6d313d607d6b44532a
ivolake <bzakhir@gmail.com>
2021-12-10 02:39:50 +0300

    Добавлен модуль для работы с расчетом значений выражений. Обновлен файл requirements.txt
    Arithmetics.py: в модуле описан класс Calculator,
    позволяющий производить вычисления относительно всех
    использованных переменных. Содержит sympy.
    
    functions.py: minors.

---
+++ Arithmetics.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import numpy as np
+from sympy import sympify, Symbol
+
+class Calculator:
+    def __init__(self):
+        self.__variables: dict = {}
+        pass
+
+    def __repr__(self):
+        return f'Calculator(variables={self.__variables})'
+
+    @property
+    def variables(self):
+        return self.__variables
+
+    def set_variable(self,
+                     var: str,
+                     val: int | float = None,
+                     symbol: Symbol = None):
+        if val is None:
+            val = self.__variables.get(var, {}).get('value', None)
+        if symbol is None:
+            symbol = self.__variables.get(var, {}).get('symbol', None)
+
+        self.__variables.update({
+            var: {
+                'value': val,
+                'symbol': symbol
+            }
+        })
+
+    def get_variable(self,
+                     var: str):
+        variable = self.__variables.get(var, {})
+        if variable == {}:
+            print(f'Error: переменная {var} отсутствует в оперативной памяти')
+        return variable
+
+    def evaluate(self,
+                 expression: str,
+                 data: list | dict = None):
+        if expression == 'avg':
+            return np.mean(data)
+        elif expression == 'msd':
+            avg = np.mean(data)
+            return (sum([(x - avg) ** 2 for x in data]) / len(data)) ** 0.5
+        elif expression == 'sum':
+            return sum(data)
+        elif expression == 'min':
+            return min(data)
+        elif expression == 'max':
+            return max(data)
+        elif expression == 'countd':
+            return np.unique(data)
+        else:
+            formulae = sympify(expression)
+            formulae_vars = {var.name: var for var in formulae.free_symbols}
+            to_substitute = {}
+            for var_name in formulae_vars:
+                value = self.get_variable(var_name).get('value', 0)
+                # symbol = self.__variables.get(var_name, {}).get('symbol', None)
+                # if symbol is None:
+                #     self.set_variable(var=var_name,
+                #                       symbol=formulae_vars[var_name])
+                to_substitute.update({formulae_vars[var_name]: value})
+            return float(formulae.evalf(subs=to_substitute))
