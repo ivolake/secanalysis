90ea6584897ac417148050bc6efa430bbf2c5410
ivolake <bzakhir@gmail.com>
2021-12-26 13:25:31 +0300

    Добавлено логгирование ошибок

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import logging
 import sys
+import traceback
 
 from PyQt6.QtWidgets import QApplication
 
@@ -6,10 +8,15 @@ from GUI import Form
 # from functions import parse_args, get_yaml
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Form()
-    ex.show()
-    sys.exit(app.exec())
+    try:
+        app = QApplication(sys.argv)
+        ex = Form()
+        ex.show()
+        sys.exit(app.exec())
+    except Exception as e:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        logging.error(''.join(traceback.format_exception(exc_type, exc_value, exc_traceback)))
+        raise e
 
     # args = parse_args(sys.argv[1:])
     # config = get_yaml(args.config_path)
